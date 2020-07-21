0daadddf2af1c0c94d5353c00f9a387c55a80209
Cerridwen <ogriff17@gmail.com>
2020-07-21 11:44:49 -0400

    made file rearrangements

---
+++ Routes/index.js
@@ -0,0 +1,3 @@
+//Router.GET---require express, models follow outline: go to 18-mongo and mongoose--routes follow along for backend. 
+//Route that to how the react app is done. Go back to hw 20 (Google Books)---this is the index.js calling index page on client side. 
+//this will help to re-structure the pages. otherwise heroku will not launch. 
